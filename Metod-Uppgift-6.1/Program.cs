0b9768f1277f0c9197ea23d1730c8aabdd318de9
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-14 10:30:44 +0100

    Add project files.

---
+++ Metod-Uppgift-6.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift6_1
+{
+    class Program
+    {
+        static void Main ()
+        {
+            Console.WriteLine(Addera(10, 10, 10));
+        }
+
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+    }
+}
